26eb095d73da0486862ae3faf0ca542a338a97d9
Himanshu-ABES <himanshu.22b1541059@abes.ac.in>
2025-04-30 12:18:00 +0530

    css error

--- frontend/src/components/UpdateBook.jsx
+++ frontend/src/components/UpdateBook.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './UpdateBook.css';
+// import './UpdateBook.css';
 
 const UpdateBook = () => {
   const [books, setBooks] = useState([]);
